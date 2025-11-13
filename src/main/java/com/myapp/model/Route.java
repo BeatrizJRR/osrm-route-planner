e100b0563ac434c46d224a9c71f11b6daaeb9e39
Frederico Peres Correia <fredericoperescorreia@hotmail.com>
2025-11-13 17:55:44 +0000

    route and poi class

--- src/main/java/com/myapp/model/Route.java
+++ src/main/java/com/myapp/model/Route.java
@@ -1,5 +1,33 @@
 package com.myapp.model;
 
+import java.util.List;
+
 public class Route {
-    
+    private final double distanceKm;
+    private final long durationSec;
+    private final TransportMode mode;
+    private final List<POI> pois;
+
+    public Route(double distanceKm, long durationSec, TransportMode mode, List<POI> pois) {
+        this.distanceKm = distanceKm;
+        this.durationSec = durationSec;
+        this.mode = mode;
+        this.pois = pois;
+    }
+
+    public double getDistanceKm() {
+        return distanceKm;
+    }
+
+    public long getDurationSec() {
+        return durationSec;
+    }
+
+    public TransportMode getMode() {
+        return mode;
+    }
+
+    public List<POI> getPois() {
+        return pois;
+    }
 }
